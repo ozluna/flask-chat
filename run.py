978ca3d1c97ded52eee2d80f58a5633c5c2d3bbd
Ozge <ozonel883@hotmail.com>
2020-07-19 16:19:46 +0000

    adding new chat html and make it useful

--- run.py
+++ run.py
@@ -1,23 +1,41 @@
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomstring1234"
+messages = []
 
 
-@app.route('/')
+def add_messages(username, message):
+    now = datetime.now().strftime("%H:%M:%S")
+    messages_dict = {"timesstamp": now, "from": username, "message": message}
+    # messages.append("({}) {}: {}".format(now, username, message))
+    messages.append(messages_dict)
+
+
+@app.route('/', methods=["GET", "POST"])
 def index():
     """Main page with instruction"""
-    return "To send a message use /Username/messsage"
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+    """if the username variable is is set, then instead of
+     returning our index.html template, we're going to redirect
+      to the contents of the session username variable."""
+    if "username" in session:
+        return redirect(session["username"])
+    return render_template("index.html")
 
 
 @app.route('/<username>')
 def user(username):
-    return "Hi "+username
+    return render_template("chat.html", username=username, chat_messages=messages)
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    add_messages(username, message)
+    return redirect("/" + username + message)
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True, load_dotenv=True)
